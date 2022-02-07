ad124ea56f3b36aaa348f3c4c8eae4890ed675a6
Felipe de Castro Teixeira Carvalho <felipecastrotc@gmail.com>
2022-02-07 17:01:22 -0300

    move the dev_mbg_lib to lib_mbg

--- lib_mbg.jl
+++ lib_mbg.jl
@@ -1,7 +1,13 @@
-using ModelingToolkit, DifferentialEquations, LinearAlgebra
-using Symbolics
+using ModelingToolkit, Plots, DifferentialEquations, LinearAlgebra
+using Symbolics, Symbolics.Latexify
+import ModelingToolkit: states, parameters
+
 
 include("lib_bg.jl")
+
+# =============================================================================
+# Types
+const planar_labels = ["x", "y", "θ"]
 
 # =============================================================================
 # Types
@@ -12,74 +18,13 @@ struct ODESystem2D{T}
     θ::T
 end
 
-
-# =============================================================================
-# Ports
-
-function Junction12D(ps2d...; name, subsys2d = [], couple = true, sgn = 1)
-    # Check subsys type
-    sys2d = subsys2d isa ODESystem2D ? [subsys2d] : subsys2d
-
-    labels = ["x", "y", "θ"]
-    sys, psucouple = [], []
-    for i in 1:length(labels)
-        namei = Symbol((name |> String) * labels[i])
-
-        ps = [getproperty(p, Symbol(labels[i])) for p in ps2d]
-        subsys = [getproperty(s, Symbol(labels[i])) for s in sys2d]
-
-        push!(sys,
-            Junction1(ps...; name = namei, subsys = subsys, couple = couple, sgn = sgn)
-        )
-    end
-
-    ODESystem2D(sys...)
-
-end
-
-function Junction02D(ps2d...; name, subsys2d = [], couple = true, sgn = 1)
-    # Check subsys type
-    sys2d = subsys2d isa ODESystem2D ? [subsys2d] : subsys2d
-
-    labels = ["x", "y", "θ"]
-    sys, psucouple = [], []
-    for i in 1:length(labels)
-        namei = Symbol((name |> String) * labels[i])
-
-        ps = [getproperty(p, Symbol(labels[i])) for p in ps2d]
-        subsys = [getproperty(s, Symbol(labels[i])) for s in sys2d]
-
-        push!(sys,
-            Junction0(ps...; name = namei, subsys = subsys, couple = couple, sgn = sgn)
-        )
-    end
-
-    ODESystem2D(sys...)
-
-end
-
-# =============================================================================
-# Modulated Transformers
-
-function mTF2Dtrans(a, θ, d)
-    [
-        1 0 a*[-sin(θ) cos(θ)]*d
-        0 1 a*[-cos(θ) -sin(θ)]*d
-        0 0 1
-    ]
-end
-
-function mTF2Dtrans(p::Dict)
-    M2Dtrans(p["a"], p["θ"], p["d"])
-end
-
 # =============================================================================
 # Elements
 
 # -----------------------------------------------------------------------------
 # Planar elements
 function Mass2D(; name, m::Vector = [1.0, 1.0, 1.0], u::Vector = [0.0, 0.0, 0.0])
-    labels = ["x", "y", "ω"]
+    labels = planar_labels
     sys = []
     for i in 1:length(m)
 
@@ -101,7 +46,7 @@ function Mass2D(; name, m::Vector = [1.0, 1.0, 1.0], u::Vector = [0.0, 0.0, 0.0]
 end
 
 function Spring2D(; name, k::Vector = [1.0, 1.0, 1.0], x::Vector = [0.0, 0.0, 0.0])
-    labels = ["x", "y", "ω"]
+    labels = planar_labels
     sys = []
     for i in 1:length(k)
         namei = Symbol((name |> String) * labels[i])
@@ -115,6 +60,7 @@ function Spring2D(; name, k::Vector = [1.0, 1.0, 1.0], x::Vector = [0.0, 0.0, 0.
         eqs = [
             e ~ q / C
             D(q) ~ f
+            # D(e) ~ f/C
         ]
 
         push!(sys, extend(ODESystem(eqs, t, [q], ps; name = namei), power))
@@ -125,7 +71,7 @@ end
 
 function Damper2D(; name, c::Vector = [1.0, 1.0, 1.0])
 
-    labels = ["x", "y", "ω"]
+    labels = planar_labels
     sys = []
     for i in 1:length(c)
         namei = Symbol((name |> String) * labels[i])
@@ -144,3 +90,223 @@ function Damper2D(; name, c::Vector = [1.0, 1.0, 1.0])
     ODESystem2D(sys...)
 
 end
+
+# =============================================================================
+# Ports
+function Junction12D(ps2d...; name, subsys2d = [], couple = true, sgn = 1)
+    # Check subsys type
+    sys2d = subsys2d isa ODESystem2D ? [subsys2d] : subsys2d
+
+    labels = planar_labels
+    sys = []
+    for i in 1:length(labels)
+        namei = Symbol((name |> String) * labels[i])
+
+        ps = getcomponent(ps2d, labels[i])
+        subsys = getcomponent(sys2d, labels[i])
+
+        push!(sys,
+            Junction1(ps...; name = namei, subsys = subsys, couple = couple, sgn = sgn)
+        )
+    end
+
+    ODESystem2D(sys...)
+
+end
+
+function Junction02D(ps2d...; name, subsys2d = [], couple = true, sgn = 1)
+    # Check subsys type
+    sys2d = subsys2d isa ODESystem2D ? [subsys2d] : subsys2d
+
+    labels = planar_labels
+    sys = []
+    for i in 1:length(labels)
+        namei = Symbol((name |> String) * labels[i])
+
+        ps = getcomponent(ps2d, labels[i])
+        subsys = getcomponent(sys2d, labels[i])
+
+        push!(sys,
+            Junction0(ps...; name = namei, subsys = subsys, couple = couple, sgn = sgn)
+        )
+    end
+
+    ODESystem2D(sys...)
+
+end
+
+# =============================================================================
+# Modulated Transformers
+
+function M2Dtrans(a, θ, d)
+    [
+        1 0 a*[-sin(θ) cos(θ)]*d
+        0 1 a*[-cos(θ) -sin(θ)]*d
+        0 0 1
+    ]
+end
+
+function M2Dtrans(p::Dict)
+    M2Dtrans(p["a"], p["θ"], p["d"])
+end
+
+function mTF2Dtrans(subsys...; name, a = 1.0, θ = 0.0, d = [0.0, 0.0])
+
+    labels = planar_labels
+
+    # Get connections
+    c = subsys isa ODESystem2D ? [subsys, nothing] : collect(subsys)
+
+    # Remove nothing from c array
+    pos = .!isnothing.(c)
+    c = c[pos]
+    @assert !isempty(c)
+
+    # If only one subsys is passed it automatically generates an open  
+    # connection
+    if sum(pos) == 1
+        sys_vec = ODESystem[]
+        for l in labels
+            namei = Symbol((name |> String) * l)
+
+            @named power = Power()
+            @unpack e, f = power
+
+            push!(sys_vec, extend(ODESystem(Equation[], t, [], []; name = namei), power))
+        end
+        sys = ODESystem2D(sys_vec...)
+
+        # Set variables according to the position
+        if pos[1]
+            E₁, F₁ = getvecvar(sys, "e"), getvecvar(sys, "f")
+            E₂, F₂ = getvecvar(c[1], "e"), getvecvar(c[1], "f")
+        else
+            E₂, F₂ = getvecvar(sys, "e"), getvecvar(sys, "f")
+            E₁, F₁ = getvecvar(c[1], "e"), getvecvar(c[1], "f")
+        end
+    else
+        @assert length(c) == 2
+        E₁, F₁ = getvecvar(c[1], "e"), getvecvar(c[1], "f")
+        E₂, F₂ = getvecvar(c[2], "e"), getvecvar(c[2], "f")
+    end
+
+    # Translation matrix
+    M = M2Dtrans(a, θ, d)
+
+    # Transformer equation
+    eqs = []
+    push!(eqs, F₂ .~ M * F₁)
+    push!(eqs, E₁ .~ M' * E₂)
+    eqs = collect(zip(eqs...))
+
+    sys_vec = []
+    for i in 1:length(labels)
+        l = labels[i]
+        namei = Symbol((name |> String) * l)
+        sym = Symbol(l)
+    
+        num_vars = [x for x in (a, θ, d[1], d[2]) if x isa Num]
+        sts = [x for x in num_vars if !isindependent(x)]
+        ps = [x for x in num_vars if isindependent(x)]
+    
+        if @isdefined sys
+            updt_sys = getproperty(sys, sym)
+    
+            eq = collect(eqs[i])
+            updt_sys = extend(ODESystem(eq, t, sts, ps; name = namei), updt_sys)
+            updt_sys = compose(updt_sys, [getproperty(j, sym) for j in c]...)
+    
+            push!(sys_vec, updt_sys)
+        else
+            eq = collect(eqs[i])
+            updt_sys = ODESystem(eq, t, sts, ps; name = namei)
+            updt_sys = compose(updt_sys, [getproperty(j, sym) for j in c]...)
+            push!(sys_vec, updt_sys)
+        end
+    end
+
+    ODESystem2D(sys_vec...)
+end
+
+# =============================================================================
+# Utils
+struct SgnODESystem2D
+    x::ODESystem
+    y::ODESystem
+    θ::ODESystem
+    sgn::Num
+end
+
+-(sys::ODESystem2D) = SgnODESystem2D(sys.x, sys.y, sys.θ, -1)
++(sys::ODESystem2D) = SgnODESystem2D(sys.x, sys.y, sys.θ, 1)
+
+function getvecvar(sys::ODESystem2D, var::String)
+    sym = Symbol(var)
+    [getproperty(getproperty(sys, Symbol(l)), sym) for l in planar_labels]
+end
+
+function getcomponent(sys, comp::String)
+
+    sgn = Symbol("sgn")
+    component = Symbol(comp)
+
+    out = SgnODESystem[]
+    for sub in sys
+        c = getproperty(sub, component)
+    
+        if !hasproperty(sub, sgn)
+            push!(out, +c)
+        else
+            if getproperty(sub, sgn) > 0
+                push!(out, +c)
+            else
+                push!(out, -c)
+            end
+        end
+    
+    end
+
+    out
+
+end
+
+function setveceq(sys::ODESystem2D, var::String)
+    sym = Symbol(var)
+    [getproperty(getproperty(sys, Symbol(l)), sym) for l in planar_labels]
+end
+
+function get2Dsystem(sys::ODESystem2D; name)
+
+    sts = states(sys)
+    ps = parameters(sys)
+    eqs = equations(sys)
+
+    ODESystem(eqs, t, sts, ps, name = name)
+end
+
+# -----------------------------------------------------------------------------
+# Extending
+function equations(sys::ODESystem2D)
+    eqs = Equation[]
+    for l in planar_labels
+        push!(eqs, equations(getproperty(sys, Symbol(l)))...)
+    end
+    eqs
+end
+
+function states(sys::ODESystem2D)
+    sts = []
+    for l in planar_labels
+        push!(sts, states(getproperty(sys, Symbol(l)))...)
+    end
+    sts
+end
+
+function parameters(sys::ODESystem2D)
+    ps = []
+    for l in planar_labels
+        push!(ps, parameters(getproperty(sys, Symbol(l)))...)
+    end
+    ps
+end
+
